1ac00a988db501565cd518d9014d68421fc9d67d
Your Name <your.email@example.com>
2026-03-04 21:34:40 +0530

    Initial commit

---
+++ OOPSBANNERApp6.java
@@ -0,0 +1,55 @@
+public class OOPSBANNERApp6 {
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] bannerLines = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            bannerLines[i] = o[i] + "   " + o[i] + "   " + p[i] + "   " + s[i];
+        }
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "  *****  "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                "******** ",
+                "**     **",
+                "**     **",
+                "******** ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                " ********",
+                "**       ",
+                "**       ",
+                " ********",
+                "       **",
+                "       **",
+                " ********"
+        };
+    }
+}
